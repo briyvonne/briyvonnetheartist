32caefe8639abe7a9cee0ce8e0e928bb0955a527
briyvonne <briyvonne@briyvonnetheartist.com>
2024-10-18 19:03:37 +0000

    added a test home page

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+const Home: React.FC = () => {
+    return (
+        <div> 
+           <h1>Welcome to Bri Yvonne's Homepage</h1>
+        </div>
+      );
+}
+
+export default Home;
+  
